264f4a06a2f1544584d394bc53ac6470dbc76b0b
Tanya Yenakieva <yastigma@gmail.com>
2024-09-20 23:52:38 +0300

    navigation correct

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
 import css from './App.module.css'
 import Logo from '../components/Logo/Logo'
 import Navigation from '../components/Navigation/Navigation'
+import Home from '../pages/Home/Home'
+import Task from '../pages/Task/Task'
+import Contact from '../pages/Contact/Contact'
+import Calendar from '../pages/Calendar/Calendar'
+import Account from '../pages/Account/Account'
+import Setting from '../pages/Setting/Setting'
+import NotFound from '../pages/NotFound/NotFound'
+
 export default function App() {
   
   return (
     <>
       <main className={css.layout}>
+      <div className={css.sidebar}>
       <Logo/>
       <Navigation/>
-         
+      </div>
+      <div className={css.content}>
+      <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/task' element={<Task/>}/>
+<Route path='/contact' element={<Contact/>}/>
+<Route path='/calendar' element={<Calendar/>}/>
+<Route path='/account' element={<Account/>}/>
+<Route path='/setting' element={<Setting/>}/>
+<Route path="*" element={<NotFound />} />
+      </Routes>
+ </div>
       </main>
       </>
   )
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,40 +1,54 @@
 import { Button } from "../Button/Button"
 import css from './Navigation.module.css' ;
-
+import { NavLink, useLocation} from "react-router-dom";
 
 export default function Navigation() {
+    const { pathname } = useLocation();
    return(   
 <div className={css.container}>
-
-    <div >
-    <p className={css.text}>Menu</p>
-    <ul className={css.menu}>
-        <li><Button> <img src="../../public/home.svg" alt="home" /> Номе </Button></li>
-    <li><Button> <img src="../../public/board.svg" alt="taskboard" /> Task </Button></li>
-
-<li><Button> <img src="../../public/contacts.svg" alt="contact" /> Contact </Button></li>
-
-<li><Button> <img src="../../public/calendar.svg" alt="calendar" /> Calendar </Button></li>
-    </ul>
-
-
-   </div> 
-
-    <div>
+<p className={css.text}>Menu</p>
+<nav className={css.menu}>
+    <NavLink to='/'>
+    <Button selected = {pathname =='/' ? true : false }> 
+    <img src="../../public/home.svg" alt="home" /> 
+    Номе 
+    </Button>
+    </NavLink>
+    <NavLink to='/task'>
+    <Button selected = {pathname =='/task' ? true : false }>
+     <img src="../../public/board.svg" alt="taskboard" /> 
+     Task 
+     </Button>
+    </NavLink>
+    <NavLink to='/contact'>
+    <Button selected = {pathname =='/contact' ? true : false }> 
+    <img src="../../public/contacts.svg" alt="contact" /> 
+    Contact 
+    </Button>
+    </NavLink>
+    <NavLink to='/calendar'>
+    <Button selected = {pathname =='/calendar' ? true : false }> 
+    <img src="../../public/calendar.svg" alt="calendar" /> 
+    Calendar 
+    </Button>
+    </NavLink>
+</nav>
     <p className={css.text}>Account</p> 
-    <ul className={css.account}>
-    <li>
-    <Button> <img src="../../public/account.svg" alt="account" /> Account </Button>
-    </li>
-        <li>
-        <Button> <img src="../../public/setting.svg" alt="setting" /> Setting </Button>
-        </li>
-    </ul>
-    </div>
-   
-   
-   
-   </div>
+<nav className={css.account}>
+    <NavLink to='/account'>
+    <Button selected = {pathname =='/account' ? true : false }> 
+    <img src="../../public/account.svg" alt="account" /> 
+    Account 
+    </Button>
+    </NavLink>
+    <NavLink to='/setting'>
+    <Button selected = {pathname =='/setting' ? true : false }>
+     <img src="../../public/setting.svg" alt="setting" /> 
+     Setting 
+     </Button>
+     </NavLink>
+   </nav>
+</div>
     
 
 ) 
